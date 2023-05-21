5dd9150563831befac6ecc12bcf74fda1c97f7c2
Victor Badanau <126888251+theinvestigattor@users.noreply.github.com>
2023-05-21 14:57:00 +0300

    get regex pattern validation for phone number and password

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function checkPassword(form) {
     const password = form.password.value;
     const confirmPassword = form.confirmPassword.value;
     if (password != confirmPassword) {
-        alert("Error! Password did not match.");
+        alert("Passwords do not match!");
         return false;
       } else {
         return true;
